fecec6b24405eba0f838dc80d8398fbab8eca79b
Allie Siclari <alliesiclari@yahoo.com>
2024-10-02 10:25:28 -0400

    Commit Model Writing Activity

---
+++ index.js
@@ -0,0 +1,38 @@
+
+//UVM
+
+//Properties:
+    //Name: UVM
+    //University Type: Public
+    //Founding Date: 1791
+    //Location: Burlington, Vermont
+    //Tuition Price, Out of state: 61,000
+    //Tuition Price, In state: 36,000
+    //Number of students: 11,898
+    //Number of majors offered: 150
+    //Number of courses offered: 500
+
+//Events:
+    //Add a course from registrar
+    //Drop a course from registrar
+    //Increase tuition rate in state
+    //Decrease tuition rate in state
+    //Increase tuition rate out of state
+    //Decrease tuition rate out of state
+    //Add a student
+    //Remove a student
+    //Add a major
+    //Remove a major
+
+//Methods:
+    //add_course: increases value of courses offered property
+    //drop_course: decreases value of courses offered property
+    //increase_tuition_in: increases value of tuition price in state property
+    //decrease_tuition_in: decreases value of tuition price in state property
+    //increase_tuition_out: increases value of tuition price out of state property
+    //decrease_tuition_out: decreases value of tuition price out of state property
+    //add_student: increases value of number of students property
+    //remove_student: decreases value of number of students property
+    //add_major: increases value of number of majors offered property
+    //remove_major: decreases value of number of majors offered property
+
